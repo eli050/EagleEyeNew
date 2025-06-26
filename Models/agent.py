d8724e20368e497673c3b7db4615d03d97cd617e
eli <lykkh78@gmail.com>
2025-06-26 13:21:58 +0300

    fixing

--- Models/agent.py
+++ Models/agent.py
@@ -4,12 +4,12 @@ from Models.enums import Status
 class Agent:
     def __init__(self, code_name:str, real_name:str, location:str,
                  status:str = Status.ACTIVE, missions_completed:int = 0 ):
-        self.code_name = code_name
-        self.real_name = real_name
-        self.location = location
-        self.status = status.lower()
+        self.code_name:str = code_name
+        self.real_name:str = real_name
+        self.location:str = location
+        self.status:str = status.lower()
         self._valid_status()
-        self.missions_completed = missions_completed
+        self.missions_completed:int = missions_completed
     def _valid_status(self):
         while not Status.__contains__(self.status):
             self.status = input("Status does not exist, please enter again:\n").lower()
