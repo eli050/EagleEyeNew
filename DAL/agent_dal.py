314cb36a4f862c5408ae90a7652c1f381688702b
eli <lykkh78@gmail.com>
2025-06-28 21:58:08 +0300

    adding get_agents method and delete agent method

--- DAL/agent_dal.py
+++ DAL/agent_dal.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 from DB.my_sql_data import MySqlData
 from Models.agent import Agent
 
@@ -42,7 +44,49 @@ class AgentDAL:
                 print("Failed to connect to the database.")
 
     @staticmethod
-    def delete_agent_by_id(agent:Agent, id:int):
-        pass
+    def delete_agent_by_id(id:int):
+        query = f"DELETE FROM agents WHERE id = {id}"
+        with MySqlData.get_connection() as conn:
+            if conn and conn.is_connected():
+                with conn.cursor() as cmd:
+                    cmd.execute(query)
+                    conn.commit()
+                    print("The deletion was successful.")
+            else:
+                print("Failed to connect to the database.")
+
+    @staticmethod
+    def get_agents():
+        query = "SELECT * FROM agents"
+        with MySqlData.get_connection() as conn:
+            if conn and conn.is_connected():
+                with conn.cursor(dictionary=True) as cmd:
+                    cmd.execute(query)
+                    dict_agents = cmd.fetchall()
+                    list_agents = list()
+                    for agent in dict_agents:
+                        list_agents.append(Agent(agent["codeName"], agent["realName"], agent["location"],
+                          agent["status"], agent["missionsCompleted"], agent["id"]))
+                    return list_agents
+
+            else:
+                print("Failed to connect to the database.")
+
+
+    @staticmethod
+    def get_agent_by_code_name(code_name:str):
+        query = f"SELECT * FROM agents WHERE codeName = '{code_name}'"
+        with MySqlData.get_connection() as conn:
+            if conn and conn.is_connected():
+                with conn.cursor(dictionary=True) as cmd:
+                    cmd.execute(query)
+                    dict_agents = cmd.fetchall()
+                    list_agents = list()
+                    for agent in dict_agents:
+                        list_agents.append(str(Agent(agent["codeName"], agent["realName"], agent["location"],
+                                                 agent["status"], agent["missionsCompleted"], agent["id"])))
+                    return list_agents
+
+
 
 
